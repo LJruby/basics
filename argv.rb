0c411674004ea2494986ce64099cdd10df5ac3bc
LJruby <labuda.jakub@gmail.com>
2015-01-27 21:06:01 +0100

    modified:   argv.rb

--- argv.rb
+++ argv.rb
@@ -21,6 +21,36 @@ when "5"
   puts "
   Podaj elementy tablicy (bez znaków cudzysłowia i przecinków):"
   puts "Tablica odwrócona: #{$stdin.gets.split.reverse}"
+when "6"
+  puts "
+  Podaj napis:"
+  puts "Pierwsza litera napisu to: #{$stdin.gets[0]}"  
+when "7"
+  puts "
+  Podaj swój wiek:"
+  if $stdin.gets.to_i > 17 
+  puts "Jesteś pełnoletni"
+  else 
+  puts "Jesteś niepełnoletni" 
+  end
+when "8"
+  dictionary={"Hello"=>"Cześć", "world"=>"świat"}
+  puts "
+  Oto dostępny słownik:
+  #{dictionary.keys}"
+  puts "
+  Podaj któryś z powyższych wyrazów aby zobaczyć tłumaczenie:
+  "
+  puts dictionary[$stdin.gets.chomp]
+when "9"
+  tablica=["Kasia", "Basia", "Ania", "Maja", "Gosia"]
+  puts "
+  Oto dostępna tablica:
+  #{tablica}"
+  puts "
+  Podaj indeks elementu (od 0 do #{tablica.length-1})
+  "
+  puts tablica[$stdin.gets.chomp.to_i]
   
 else
   puts "
@@ -32,6 +62,10 @@ else
   2: Odwróć napis
   3: Długość napisu
   4: Powiel napis
-  5: Odwróć tablicę"
+  5: Odwróć tablicę
+  6: Pierwsza litera napisu
+  7: Pełnoletni?
+  8: Mini słownik
+  9: Mini tablica"
 
 end
